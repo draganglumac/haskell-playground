c2f881ca056ffbf74cfc43eab22e3fdd2f698189
Dragan Glumac <dragan.glumac@gmail.com>
2018-04-02 22:04:50 +0100

    Sequencing parsers.

--- monadic-parsers/src/MonadicParsers.hs
+++ monadic-parsers/src/MonadicParsers.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module MonadicParsers where
 
 import Control.Applicative
@@ -13,3 +15,24 @@ item :: Parser Char
 item = P (\input -> case input of
                     [] -> []
                     (x:xs) -> [(x, xs)])
+
+instance Functor Parser where
+  --fmap :: (a -> b) -> Parser a -> Parser b
+  fmap g p = P (\input -> case parse p input of
+                          [] -> []
+                          [(v, output)] -> [(g v, output)])
+
+instance Applicative Parser where
+  --pure :: a -> Parser a
+  pure x = P (\inp -> [(x, inp)])
+
+  --(<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  pg <*> px = P (\inp -> case parse pg inp of
+                         [] -> []
+                         [(g, out)] -> parse (fmap g px) out)
+
+instance Monad Parser where
+  -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  p >>= f = P (\inp -> case parse p inp of
+                       [] -> []
+                       [(v, out)] -> parse (f v) out)
