07595468dee880c7da8d7f4c1876db46aa635582
Dragan Glumac <dragan.glumac@gmail.com>
2018-04-03 12:52:11 +0100

    WIP Calculator

--- monadic-parsers/src/Calculator.hs
+++ monadic-parsers/src/Calculator.hs
@@ -1,3 +1,30 @@
 module Calculator where
 
 import MonadicParsers
+
+box :: [String]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | _ |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
+
+buttons :: String
+buttons = standard ++ extra
+  where
+    standard = "qcd=123+456-789*0()/"
+    extra = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO ()
+showbox = sequence_ [writeat (1, y) b | (y, b) <- zip [1..] box]
+
+display xs = do writeat (3, 2) (replicate 13 ' ')
+                writeat (3, 2) (reverse (take 13 (reverse xs)))
